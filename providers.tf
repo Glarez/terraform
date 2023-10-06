2d29f8f2aee8ee52e11b7f411250de02d5f71a9d
German Larez <germanlarez@gmail.com>
2023-10-06 07:46:17 -0300

    commented backend config

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 5.19.0"
     }
   }
-  backend "s3" {
+  /*backend "s3" {
    bucket         = "terraform-s3-remote-state-693414"
    key            = "global/s3/terraform.tfstate"
    region         = "us-west-2"
@@ -13,5 +13,5 @@ terraform {
    dynamodb_table = "terraform-lock-table"
    encrypt        = true
   }
-  required_version = ">= 1.5.0"
+  */required_version = ">= 1.5.0"
 }
