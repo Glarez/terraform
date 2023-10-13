6ffc466198ad07c6b36de007855f1fb32a7c4d27
German Larez <germanlarez@gmail.com>
2023-10-12 21:44:08 -0300

    updated provider

--- providers.tf
+++ providers.tf
@@ -8,13 +8,4 @@ terraform {
   
   required_version = ">= 1.5.0"
   
-  /*backend "s3" {
-   bucket         = "terraform-s3-remote-state-693414"
-   key            = "global/s3/terraform.tfstate"
-   region         = "us-west-2"
-
-   dynamodb_table = "terraform-lock-table"
-   encrypt        = true
-  }
-  */
 }
