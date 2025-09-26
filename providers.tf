80366f4654c7a2d597934cf73ccc6e2037099bea
Glarez <germanlarez@gmail.com>
2025-09-26 09:51:10 -0300

    initial aws

--- providers.tf
+++ providers.tf
@@ -2,24 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "~> 6.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "~> 3.0"
     }
   }
-  required_version = "~> 1.0"
+  required_version = "~> 1.13"
 
-  backend "remote" {
+   backend "remote" {
     organization    = "Zapalaver"
-
-    workspaces {
-      name = "zapalaver-dev"
+   workspaces {
     }
+      name = "terraform-github"
   }
 }
-
 
 provider "aws" {
   region = "us-east-1"
